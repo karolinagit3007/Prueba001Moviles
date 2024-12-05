12c273985502ba710e8be0b8ba9d23537f0ff702
karolinagit3007 <karolina.17hidalgo@gmail.com>
2024-12-05 08:09:08 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:prueba_001/screens/Pantalla2.dart'; 
+import 'package:prueba_001/screens/Pantalla2.dart';
 import '../navigadores/drawer.dart';
 
 void main() {
@@ -32,77 +32,85 @@ class Cuerpo extends StatelessWidget {
             fontSize: 24,
           ),
         ),
-        backgroundColor: const Color.fromRGBO(224, 51, 51, 1),
+        backgroundColor: const Color.fromARGB(255, 12, 120, 207),
       ),
       drawer: MiDrawer(),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 150),
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  children: const [
-                    Text(
-                      "Nombre: Karolina Gavilema",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Usuario de GitHub: karolinagit3007",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton.icon(
-                onPressed: () => navegar02(context),
-                icon: const Icon(Icons.navigate_next_outlined, color: Colors.white),
-                label: const Text(
-                  "Ir a la página 2",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(224, 51, 51, 1),
-                  shape: RoundedRectangleBorder(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/main.webp',
+            height: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 150),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 112, 8, 8).withOpacity(0.5),
                     borderRadius: BorderRadius.circular(12),
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage("assets/images/main.webp"),
-                    fit: BoxFit.cover,
+                  child: Column(
+                    children: const [
+                      Text(
+                        "Nombre: Karolina Gavilema",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Usuario de GitHub: karolinagit3007",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                  borderRadius: BorderRadius.circular(12),
                 ),
-              ),
-            ],
+                const SizedBox(height: 40),
+                ElevatedButton.icon(
+                  onPressed: () => navegar02(context),
+                  icon: const Icon(Icons.navigate_next_outlined, color: Colors.white),
+                  label: const Text(
+                    "Ir a la página 2",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(224, 51, 51, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  height: 220,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/main.webp"),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
